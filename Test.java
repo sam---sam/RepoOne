4ac3fde72f1e211b5845303fb30cfa8af30c384c
sam---sam <sam---sam@users.noreply.github.com>
2015-05-16 07:40:55 -0400

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,6 +1,24 @@
 
 public static void(String args[]){
 
-System.out.println("Hello World");
-//*???
+	      String s = new String ();
+	      
+	      System.out.println("Enter a string");
+	      s = new Scanner(System.in).nextLine();
+	      
+	    
+	      int count=0;
+	      
+	      
+	      for (int i=0;i<s.length();i++)
+	    	  if (s.charAt(i)=='a' || s.charAt(i)=='A' ||
+    			  s.charAt(i)=='e' || s.charAt(i)=='E' ||
+    			  s.charAt(i)=='i' || s.charAt(i)=='I' ||
+    			  s.charAt(i)=='o' || s.charAt(i)=='O' ||
+    			  s.charAt(i)=='u' || s.charAt(i)=='U' ||
+    			  s.charAt(i)=='y' || s.charAt(i)=='Y' )
+	    	count ++;  
+	    	  
+	      
+	      System.out.println("The number of vowel is : "+count);
 }
