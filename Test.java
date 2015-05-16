a6558eff0ab02bf18f3cb5c42e3b248ad41fec8a
sam---sam <sam---sam@users.noreply.github.com>
2015-05-16 19:32:48 -0400

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,6 +1,30 @@
+import java.util.Scanner;
 
-public static void(String args[]){
 
-System.out.println("Hello World");
-
+public class Test {
+	public static void main (String args[]){
+	     
+	      String s = new String ();
+	      
+	      System.out.println("Enter a string");
+	      s = new Scanner(System.in).nextLine();
+	      
+	    
+	      int count=0;
+	      
+	      
+	      for (int i=0;i<s.length();i++)
+	    	  if (s.charAt(i)=='a' || s.charAt(i)=='A' ||
+    			  s.charAt(i)=='e' || s.charAt(i)=='E' ||
+    			  s.charAt(i)=='i' || s.charAt(i)=='I' ||
+    			  s.charAt(i)=='o' || s.charAt(i)=='O' ||
+    			  s.charAt(i)=='u' || s.charAt(i)=='U' ||
+    			  s.charAt(i)=='y' || s.charAt(i)=='Y' )
+	    	count ++;  
+	    	  
+	      
+	      System.out.println("The number of vowels is : "+count);
+	      
+	      
+	}
 }
