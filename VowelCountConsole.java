9f3c96dd7a93f720357e3d19691194d1dd0a107a
sam---sam <sam---sam@users.noreply.github.com>
2015-05-18 23:10:53 -0400

    Update VowelCountConsole.java
    Same code still but with comments , so it ll be easier for to read it

--- VowelCountConsole.java
+++ VowelCountConsole.java
@@ -1,8 +1,7 @@
 
 import java.util.Scanner;
 
-public class VowelCountConsole {
-    
+public class launcher {
     
     public static void main(String[] args){
         System.out.println("Enter a string");
@@ -13,16 +12,22 @@ public class VowelCountConsole {
         int count = 0 , vList[];
         vList = new int [5];
         
-        for (int i = 0; i < word.length(); i++) {
-        	int flag = word.charAt(i) == 'a' || word.charAt(i) == 'A' ? vList[0]++: 
-        		    word.charAt(i) == 'e' || word.charAt(i) == 'E' ? vList[1]++:
-        		   	word.charAt(i) == 'i' || word.charAt(i) == 'I' ? vList[2]++: 
-                	word.charAt(i) == 'o' || word.charAt(i) == 'O' ? vList[3]++:
-                 	word.charAt(i) == 'u' || word.charAt(i) == 'U' ? vList[4]++: 
-           	-1;// -1 off
-        	int check = flag != -1 ? count++:0;
-            
+        int flag=0;// java ternary conditional expression result need to be assigned into a variable 
+        //this is an usual for loop , but it works fine 
+        for (int i = -1; // initializing loop variable ... 
+        	 i++ <  word.length()-1; //incrementing (post incrementing) and comparing it word length
+        		//this is the fun part . All of this is really equal to a bunch of if else if 
+        	 flag = word.charAt(i) == 'a' || word.charAt(i) == 'A' ? vList[0]++: 
+        	   	 	word.charAt(i) == 'e' || word.charAt(i) == 'E' ? ++vList[1]:
+        	   	 	word.charAt(i) == 'i' || word.charAt(i) == 'I' ? ++vList[2]: 
+        	   	 	word.charAt(i) == 'o' || word.charAt(i) == 'O' ? ++vList[3]:
+        	   	 	word.charAt(i) == 'u' || word.charAt(i) == 'U' ? ++vList[4]: 
+        	   	 	-1// -1 off        		 	
+        	 )
+        {
+        	int check = flag != -1 ? count++:0;//check is not used it is just a way to increment count depending on certain conditions
         }
+        
         System.out.println("The number of vowels is : "+ count);
  
         System.out.println(" a = "+ vList[0]+
@@ -30,7 +35,5 @@ public class VowelCountConsole {
         				   " i = "+ vList[2]+
         				   " o = "+ vList[3]+
         				   " u = "+ vList[4]);
-    }
-
-    
+    }    
 }
